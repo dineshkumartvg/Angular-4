fb334b53aab6b0871172b0e041ef11744b909040
mkranthikiran <47345158+mkranthikiran@users.noreply.github.com>
2019-02-18 15:08:01 +0530

    Add files via upload

---
+++ day5/userRoutes.ts
@@ -0,0 +1,7 @@
+import {Routes} from '@angular/router';
+import {UserProfileComponent} from './userprofile.component';
+import {UserLoginComponent} from './user.login.component';
+export const userRoutes : Routes = [
+    {path : 'profile', component : UserProfileComponent },
+]
+
