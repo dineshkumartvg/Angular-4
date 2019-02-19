3724fbb18a9c6c27dceeef28470a1896edf59a92
mkranthikiran <47345158+mkranthikiran@users.noreply.github.com>
2019-02-19 16:09:58 +0530

    Add files via upload

---
+++ day6/user.profile.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: `./user.profile.component.html`,
+  styles : ['em{float:right; color: #E05c65; padding-left-10px;}']
+})
+export class UserProfileComponent  {}
