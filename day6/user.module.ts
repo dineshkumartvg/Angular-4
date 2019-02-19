15c6b076eb6b7e1cb4c2f5418d54f0941f354b43
mkranthikiran <47345158+mkranthikiran@users.noreply.github.com>
2019-02-19 16:06:33 +0530

    Update user.module.ts

--- day6/user.module.ts
+++ day6/user.module.ts
@@ -1,11 +1,12 @@
 import {  CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {UserProfileComponent} from './user.profile.component'
-
+import {UserRoutingModule} from './user-routing.module'
 @NgModule({
   declarations: [UserProfileComponent],
   imports: [
     CommonModule,
+    UserRoutingModule
     ],
   providers : [],
   exports: [
