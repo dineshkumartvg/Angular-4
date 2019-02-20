a2ddef12886f3574b39baa961b8c30876cc84991
mkranthikiran <47345158+mkranthikiran@users.noreply.github.com>
2019-02-20 16:42:49 +0530

    Update user.profile.component.ts

--- day6/user.profile.component.ts
+++ day6/user.profile.component.ts
@@ -1,8 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticateService } from './authenticate.service'
 
 @Component({
   templateUrl: `./user.profile.component.html`,
   styles : ['em{float:right; color: #E05c65; padding-left-10px;}']
 })
-export class UserProfileComponent  {}
+export class UserProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  constructor(private router: Router, private authenticteService: AuthenticateService) { }
+
+  ngOnInit() {
+    let firstName = new FormControl(this.authenticteService.currentUser.firstName,Validators.required);
+    let lastName = new FormControl(this.authenticteService.currentUser.lastName,Validators.required);
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    })
+  }
+
+  saveUser(formValues) {
+    if (this.profileForm.valid) { 
+    this.authenticteService.updateUser(formValues.firstName, formValues.lastName)
+    this.router.navigate(['/courses'])
+  }
+}
+
+cancel()
+{ this.router.navigate(['/courses']) }
+}
+//this.authenticteService.currentUser.firstName, Validators.required
+//this.authenticteService.currentUser.lastName, Validators.required
